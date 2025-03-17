9ddd9a31ff378f06c7986eba041e36234f9218a0
Kamalakar30 <chitlakamalakar123@gmail.com>
2025-03-17 20:13:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["software Developer", "problem solver", "Full stack"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
